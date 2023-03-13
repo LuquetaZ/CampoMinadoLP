6395220aceec153eee0c6210b1befa5ad5dcc55d
Ranier Dalton do Couto <ranierd.couto@gmail.com>
2023-03-13 19:44:24 -0300

    1.4
    - Adicionado o projeto inicial da forma gráfica do Visual Studio.

---
+++ Arquivos/C#/CampoMinado2/CampoMinado2/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace CampoMinado2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnjogar = new System.Windows.Forms.Button();
+            this.btnsair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnjogar
+            // 
+            this.btnjogar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnjogar.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnjogar.Location = new System.Drawing.Point(176, 67);
+            this.btnjogar.Name = "btnjogar";
+            this.btnjogar.Size = new System.Drawing.Size(181, 70);
+            this.btnjogar.TabIndex = 0;
+            this.btnjogar.Text = "JOGAR";
+            this.btnjogar.UseVisualStyleBackColor = false;
+            this.btnjogar.Click += new System.EventHandler(this.btnjogar_Click);
+            // 
+            // btnsair
+            // 
+            this.btnsair.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnsair.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsair.Location = new System.Drawing.Point(176, 185);
+            this.btnsair.Name = "btnsair";
+            this.btnsair.Size = new System.Drawing.Size(181, 70);
+            this.btnsair.TabIndex = 1;
+            this.btnsair.Text = "SAIR";
+            this.btnsair.UseVisualStyleBackColor = false;
+            this.btnsair.Click += new System.EventHandler(this.btnsair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CampoMinado2.Properties.Resources.single_3214801_featured_image;
+            this.ClientSize = new System.Drawing.Size(549, 335);
+            this.Controls.Add(this.btnsair);
+            this.Controls.Add(this.btnjogar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnjogar;
+        private System.Windows.Forms.Button btnsair;
+    }
+}
+
---
+++ Arquivos/C#/CampoMinado2/CampoMinado2/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CampoMinado2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 media = new Form1();
+            this.Hide();
+            media.Show();
+        }
+    }
+}
